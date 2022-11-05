7a39ea4d9e4e310837deb5b5740c0dcc7b150892
IEEE Student Branch, SVCE <103510207+IEEE-SVCE@users.noreply.github.com>
2022-11-05 21:25:35 +0530

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,127 @@
+
+
+(function($) {
+
+    "use strict";
+    
+    var cfg = {
+        scrollDuration : 800, // smoothscroll duration
+           },
+
+    $WIN = $(window);
+    var doc = document.documentElement;
+    doc.setAttribute('data-useragent', navigator.userAgent);
+    if (!Modernizr.svg) {
+        $(".home-logo img").attr("src", "images/logo.png");
+    }
+
+
+   /* Preloader
+    * -------------------------------------------------- */
+    var ssPreloader = function() {
+        
+        $("html").addClass('ss-preload');
+
+        $WIN.on('load', function() {
+
+            
+            $("#loader").fadeOut("slow", function() {
+                
+                $("#preloader").delay(300).fadeOut("slow");
+            }); 
+            
+            $("html").removeClass('ss-preload');
+            $("html").addClass('ss-loaded');
+        
+        });
+    };
+
+
+    var ssInfoToggle = function() {
+
+               $('.info-toggle').on('click', function(event) {
+
+            event.preventDefault();
+            $('body').toggleClass('info-is-visible');
+
+        });
+
+    };
+
+
+       var ssSlickSlider = function() {
+        
+        $('.home-slider').slick({
+            arrows: false,
+            dots: false,
+            autoplay: true,
+            autoplaySpeed: 3000,
+            fade: true,
+            speed: 3000
+        });
+
+    };
+
+
+    var ssPlaceholder = function() {
+        $('input, textarea, select').placeholder();
+    };
+
+
+   /* final countdown
+    * ------------------------------------------------------ */
+    var ssFinalCountdown = function() {
+
+        var finalDate =  new Date("November 11, 2022 00:00:00").getTime();
+        //-date: "Mar 25 2021",
+
+        $('.home-content__clock').countdown(finalDate)
+        .on('update.countdown finish.countdown', function(event) {
+
+            var str = '<div class=\"top\"><div class=\"time days\">' +
+                      '%D <span>day%!D</span>' + 
+                      '</div></div>' +
+                      '<div class=\"time hours\">' +
+                      '%H <span>H</span></div>' +
+                      '<div class=\"time minutes\">' +
+                      '%M <span>M</span></div>' +
+                      '<div class=\"time seconds\">' +
+                      '%S <span>S</span></div>';
+
+            $(this)
+            .html(event.strftime(str));
+
+        });
+    };
+
+    var ssAjaxChimp = function() {
+        
+        $('#mc-form').ajaxChimp({
+            language: 'es',
+            url: cfg.mailChimpURL
+        });
+
+        $.ajaxChimp.translations.es = {
+            'submit': 'Submitting...',
+            0: '<i class="fas fa-check"></i> We have sent you a confirmation email',
+            1: '<i class="fas fa-exclamation-triangle"></i> You must enter a valid e-mail address.',
+            2: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            3: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            4: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.',
+            5: '<i class="fas fa-exclamation-triangle"></i> E-mail address is not valid.'
+        }
+    };
+
+    (function ssInit() {
+        
+        ssPreloader();
+        ssInfoToggle();
+        ssSlickSlider();
+        ssPlaceholder();
+        ssFinalCountdown();
+        ssAjaxChimp();
+
+    })();
+
+
+})(jQuery);
