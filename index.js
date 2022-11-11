d9d453979b15c908862d3cd18a370278def42980
IEEE Student Branch, SVCE <103510207+IEEE-SVCE@users.noreply.github.com>
2022-11-11 21:56:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+crossorigin="anonymous"
+
+ScrollReveal({
+    reset: true,
+    distance: "60px",
+    duration: 2500,
+    delay: 400,
+  });
+
+  ScrollReveal().reveal(".about_heading", { delay: 300, origin: "left" });
+  ScrollReveal().reveal(".about_image", { delay: 400, origin: "bottom" });
+  ScrollReveal().reveal(".about_parah", { delay: 500, origin: "right" });
+  ScrollReveal().reveal(".eve_img", { delay: 200, origin: "bottom" });
+  ScrollReveal().reveal(".eve_head", { delay: 200, origin: "left" });
+  ScrollReveal().reveal(".event_images", { delay: 200, origin: "right" });
+  ScrollReveal().reveal(".crew_head", { delay: 200, origin: "right" });
+  ScrollReveal().reveal("#a", { delay: 200, origin: "top" });
+  ScrollReveal().reveal("#b", { delay: 200, origin: "bottom" });
+  ScrollReveal().reveal("#c", { delay: 200, origin: "top" });
+  
+ 
+  
